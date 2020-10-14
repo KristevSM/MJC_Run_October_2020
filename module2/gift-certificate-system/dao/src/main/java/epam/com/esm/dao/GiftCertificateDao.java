97e9aebd6f99644977e3b986ebe9510f962c89a2
Sergei Kristev <kristevsm@gmail.com>
2020-10-15 01:55:32 +0300

    Created rowMapper in GiftCertificateDao Updated model (added id)

---
+++ module2/gift-certificate-system/dao/src/main/java/epam/com/esm/dao/GiftCertificateDao.java
@@ -0,0 +1,6 @@
+package epam.com.esm.dao;
+
+import com.epam.esm.model.GiftCertificate;
+
+public interface GiftCertificateDao extends CrudDAO<GiftCertificate> {
+}
---
+++ module2/gift-certificate-system/model/src/main/java/com/epam/esm/model/Tag.java
@@ -0,0 +1,17 @@
+package com.epam.esm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Tag {
+    private Long id;
+    private String name;
+}
