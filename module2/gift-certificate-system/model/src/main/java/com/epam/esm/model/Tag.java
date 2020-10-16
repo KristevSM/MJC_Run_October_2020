b4aa52a3a4f8ca7903782a99326929772e9997ac
Sergei Kristev <kristevsm@gmail.com>
2020-10-16 11:13:18 +0300

    Removed DTO Resolved issue with date parsing

--- module2/gift-certificate-system/model/src/main/java/com/epam/esm/model/Tag.java
+++ module2/gift-certificate-system/model/src/main/java/com/epam/esm/model/Tag.java
@@ -4,14 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Tag {
+public class Tag implements Serializable {
+    private static final long serialVersionUID = -1L;
+
     private Long id;
     private String name;
 }
