f83a040eff5dbebe88d27c39db09cf9b47fe298f
Sergei Kristev <kristevsm@gmail.com>
2020-10-16 21:51:12 +0300

    Created service interfaces

---
+++ module2/gift-certificate-system/service/src/main/java/com/epam/esm/service/GiftCertificateService.java
@@ -0,0 +1,22 @@
+package com.epam.esm.service;
+
+import com.epam.esm.model.GiftCertificate;
+import java.util.List;
+import java.util.Optional;
+
+public interface GiftCertificateService {
+
+    List<GiftCertificate> findAllCertificates();
+    Optional<GiftCertificate> findCertificateById(Long id);
+    Long saveCertificate(GiftCertificate giftCertificate);
+    void updateCertificate(GiftCertificate giftCertificate);
+    void deleteCertificate(Long id);
+
+    List<GiftCertificate> getCertificatesByTagName(String tagName);
+    List<GiftCertificate> getCertificatesByPartOfName(String partOfName);
+    List<GiftCertificate> getCertificatesByPartOfDescription(String partOfDescription);
+    void addTagToCertificate(Long certificateId, Long tagId);
+    void removeTagFromCertificate(Long certificateId, Long tagId);
+
+    void sortCertificateByParameter(String...parameters);
+}
