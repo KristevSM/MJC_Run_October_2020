6baf5b52525e58fad69cf603d8aa8874b685bb17
Sergei Kristev <kristevsm@gmail.com>
2020-10-16 21:51:12 +0300

    Created service interfaces

---
+++ module2/gift-certificate-system/service/src/main/java/com/epam/esm/service/TagService.java
@@ -0,0 +1,14 @@
+package com.epam.esm.service;
+
+import com.epam.esm.model.Tag;
+import java.util.List;
+import java.util.Optional;
+
+public interface TagService {
+
+    List<Tag> findAllTags();
+    Optional<Tag> findTagById(Long id);
+    Long saveTag(Tag tag);
+    void updateTag(Tag tag);
+    void deleteTag(Long id);
+}
