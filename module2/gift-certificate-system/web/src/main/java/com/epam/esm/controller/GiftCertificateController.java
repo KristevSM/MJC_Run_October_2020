fa3ecafbb00c209d599547cbb1bf16795c56741f
Sergei Kristev <kristevsm@gmail.com>
2020-10-18 00:36:45 +0300

    Created validators. Configured apache connection pool

--- module2/gift-certificate-system/web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
+++ module2/gift-certificate-system/web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
@@ -2,24 +2,49 @@ package com.epam.esm.controller;
 
 import com.epam.esm.model.GiftCertificate;
 import com.epam.esm.service.GiftCertificateService;
+import com.epam.esm.validator.GiftCertificateValidator;
+import com.epam.esm.validator.TagValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
+@RequestMapping("/certificates")
 public class GiftCertificateController {
 
-    @Autowired
-    private GiftCertificateService giftCertificateService;
+    private final GiftCertificateService giftCertificateService;
+    private final GiftCertificateValidator certificateValidator;
+    private final TagValidator tagValidator;
 
-    public GiftCertificateController(GiftCertificateService giftCertificateService) {
+    @Autowired
+    public GiftCertificateController(GiftCertificateService giftCertificateService, GiftCertificateValidator certificateValidator, TagValidator tagValidator) {
         this.giftCertificateService = giftCertificateService;
+        this.certificateValidator = certificateValidator;
+        this.tagValidator = tagValidator;
     }
+
+    @Autowired
+
 
     @GetMapping(value = "/gift-certificates")
     public List<GiftCertificate> findAll() {
         return giftCertificateService.findAllCertificates();
     }
+
+    @PostMapping(path = "/gift-certificates", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Integer> addGiftCertificate(@RequestBody GiftCertificate giftCertificate) {
+        giftCertificateService.saveCertificate(giftCertificate);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GiftCertificate findById(@PathVariable Long id) {
+        //todo create new exception
+        return giftCertificateService.findCertificateById(id).orElseThrow(NoSuchElementException::new);
+    }
+
 }
---
+++ module2/gift-certificate-system/web/src/test/java/com/epam/esm/validator/GiftCertificateValidatorTest.java
@@ -0,0 +1,60 @@
+package com.epam.esm.validator;
+
+import com.epam.esm.model.GiftCertificate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GiftCertificateValidatorTest {
+
+    private GiftCertificateValidator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new GiftCertificateValidator();
+    }
+
+    @Test
+    void validate() {
+        GiftCertificate validCertificate = GiftCertificate.builder()
+                .name("New certificate")
+                .description("Some description")
+                .price(BigDecimal.valueOf(100D))
+                .createDate(LocalDateTime.now())
+                .lastUpdateDate(LocalDateTime.now())
+                .duration(6)
+                .tags(new ArrayList<>())
+                .build();
+
+        BindingResult result = new BeanPropertyBindingResult(validCertificate, "giftCertificate");
+        validator.validate(validCertificate, result);
+        System.out.println("Errors count: " + result.getAllErrors().size());
+        assertFalse(result.hasErrors());
+
+        GiftCertificate inValidCertificate = GiftCertificate.builder()
+                .name("dlqbkmggdlgcekupeaoogaadudduwjdyxihdxzfpn")
+                .description("wmrjkwqcrpsndahcsdshxrrhdcylygohzxlocbhqiiueobqllipiyxjyeldnsemdymdlwexbkqwgokyhzxnwcrosr" +
+                        "smlkcmpongxzuuizdwqgnklglqobdrhpwtbudivcdjyzheefskryltblnqsleqsignrntnojzecnfuvcgjjyoahsbvgjbvo" +
+                        "kfwviqrdqaknxwglxsyfmbxxxafskegeerxbejftfhdxsgyngbetifcvyvluxklbonj\n" +
+                        "\n")
+                .price(BigDecimal.valueOf(-1D))
+                .createDate(LocalDateTime.now())
+                .lastUpdateDate(LocalDateTime.now().minusDays(1))
+                .duration(-6)
+                .build();
+
+        validator.validate(inValidCertificate, result);
+        System.out.println("Errors count: " + result.getAllErrors().size());
+        result.getAllErrors().forEach(System.out::println);
+
+        assertEquals(6, result.getAllErrors().size());
+
+    }
+}
