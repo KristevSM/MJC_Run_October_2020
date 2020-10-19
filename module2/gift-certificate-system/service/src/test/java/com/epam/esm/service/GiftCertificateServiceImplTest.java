2a4c9caa4f1e44b930632e8c0c0ccc644db7f50e
Sergei Kristev <kristevsm@gmail.com>
2020-10-19 18:20:27 +0300

    Added GiftCertificateService methods

--- module2/gift-certificate-system/service/src/test/java/com/epam/esm/service/GiftCertificateServiceImplTest.java
+++ module2/gift-certificate-system/service/src/test/java/com/epam/esm/service/GiftCertificateServiceImplTest.java
@@ -3,7 +3,9 @@ package com.epam.esm.service;
 import com.epam.esm.dao.GiftCertificateDao;
 import com.epam.esm.dao.TagDao;
 import com.epam.esm.dao.TagDaoJdbc;
+import com.epam.esm.exception.GiftCertificateNotFoundException;
 import com.epam.esm.model.GiftCertificate;
+import com.epam.esm.model.Tag;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,7 +16,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,6 +44,7 @@ class GiftCertificateServiceImplTest {
         certificateService = new GiftCertificateServiceImpl(giftCertificateDao, tagDao);
         tagDao = new TagDaoJdbc(dataSource);
     }
+
     @AfterEach
     void tearDown() {
     }
@@ -46,15 +53,49 @@ class GiftCertificateServiceImplTest {
     void shouldFindAllCertificates() {
         List<GiftCertificate> certificates = certificateService.findAllCertificates();
         int expectedListSize = 3;
+        System.out.println(certificates);
         assertEquals(expectedListSize, certificates.size());
     }
 
     @Test
-    void findCertificateById() {
+    void shouldFindCertificateById() {
+        Long expectedId = 1L;
+        GiftCertificate giftCertificate = certificateService.findCertificateById(expectedId);
+        assertEquals(expectedId, giftCertificate.getId());
+        assertThrows(GiftCertificateNotFoundException.class, () -> {
+            certificateService.findCertificateById(12L);
+        });
     }
 
     @Test
-    void saveCertificate() {
+    void shouldSaveAndRemoveCertificate() {
+        GiftCertificate certificate = GiftCertificate.builder()
+                .name("New certificate")
+                .description("Some description")
+                .price(BigDecimal.valueOf(100D))
+                .createDate(LocalDateTime.now())
+                .lastUpdateDate(LocalDateTime.now())
+                .duration(6)
+                .tags(new ArrayList<>())
+                .build();
+
+        System.out.println(certificate);
+
+        Long certificateId = certificateService.saveCertificate(certificate);
+        certificateService.addTagToCertificate(certificateId, 3L);
+
+        GiftCertificate newCertificate = certificateService.findCertificateById(certificateId);
+        System.out.println(newCertificate);
+
+        assertTrue(certificateService.findCertificateById(certificateId).getName().contains("New certificate"));
+
+        certificateService.removeTagFromCertificate(certificateId, 1L);
+        certificateService.deleteCertificate(certificateId);
+
+        assertThrows(GiftCertificateNotFoundException.class, () -> {
+            certificateService.findCertificateById(certificateId);
+        });
+
     }
 
     @Test
