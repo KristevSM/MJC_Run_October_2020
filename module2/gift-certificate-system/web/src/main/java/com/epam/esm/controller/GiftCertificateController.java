6aa50802473bd25d6b8fb1bdbcab1ca50705dac9
Sergei Kristev <kristevsm@gmail.com>
2020-10-20 01:55:58 +0300

    Fix validator implementation in certificate controller

--- module2/gift-certificate-system/web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
+++ module2/gift-certificate-system/web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
@@ -1,5 +1,7 @@
 package com.epam.esm.controller;
 
+import com.epam.esm.exception.GiftCertificateNotFoundException;
+import com.epam.esm.exception.TagNotFoundException;
 import com.epam.esm.model.GiftCertificate;
 import com.epam.esm.service.GiftCertificateService;
 import com.epam.esm.service.TagService;
@@ -16,6 +18,7 @@ import com.github.fge.jsonpatch.JsonPatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -50,32 +53,41 @@ public class GiftCertificateController {
     }
 
     @PostMapping(path = "/certificates", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Integer> addGiftCertificate(@RequestBody @Valid GiftCertificate giftCertificate, BindingResult result) {
+    public ResponseEntity<Integer> addGiftCertificate(@RequestBody @Valid GiftCertificate giftCertificate) {
+
         if (giftCertificate.getTags() == null) {
             giftCertificate.setTags(new ArrayList<>());
         }
+        BindingResult result = new BeanPropertyBindingResult(giftCertificate, "giftCertificate");
         certificateValidator.validate(giftCertificate, result);
         if (result.hasErrors()) {
             return new ResponseEntity(result.getAllErrors(), HttpStatus.BAD_REQUEST);
         } else {
             Long certificateId = giftCertificateService.saveCertificate(giftCertificate);
-            //todo create external tag properties
             tagService.assignDefaultTag("Main", certificateId);
             return ResponseEntity.ok().build();
         }
     }
 
     @PatchMapping(path = "certificates/{id}", consumes = "application/json-patch+json")
-    public ResponseEntity<GiftCertificate> updateGiftCertificate(@PathVariable Long id, @RequestBody JsonPatch patch) {
+    public ResponseEntity<GiftCertificate> updateGiftCertificate(@PathVariable Long id,
+                                                                 @RequestBody JsonPatch patch) {
 
         try {
             GiftCertificate oldCertificate = giftCertificateService.findCertificateById(id);
             GiftCertificate certificatePatched = applyPatchToGiftCertificate(patch, oldCertificate);
-            giftCertificateService.updateCertificate(certificatePatched);
-            return ResponseEntity.ok().build();
+
+            BindingResult result = new BeanPropertyBindingResult(certificatePatched, "giftCertificate");
+            certificateValidator.validate(certificatePatched, result);
+            if (result.hasErrors()) {
+                return new ResponseEntity(result.getAllErrors(), HttpStatus.BAD_REQUEST);
+            } else {
+                giftCertificateService.updateCertificate(certificatePatched);
+                return ResponseEntity.ok().build();
+            }
         } catch (JsonPatchException | JsonProcessingException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        } catch (Exception e) {
+        } catch (GiftCertificateNotFoundException | TagNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
@@ -90,24 +102,9 @@ public class GiftCertificateController {
         return objectMapper.treeToValue(patched, GiftCertificate.class);
     }
 
-//        certificateValidator.validate(giftCertificate, result);
-//        if (result.hasErrors()) {
-//            return new ResponseEntity(result.getAllErrors(), HttpStatus.BAD_REQUEST);
-//        } else {
-//            GiftCertificate oldCertificate = giftCertificateService.findCertificateById(giftCertificate.getId());
-//            if(oldCertificate.getName() != giftCertificate.getName()) {
-//
-//            }
-//            giftCertificateService.updateCertificate(giftCertificate);
-//            //todo create external tag properties
-//            tagService.updateTag("Main", certificateId);
-//            return ResponseEntity.ok().build();
-//        }
-//    }
 
     @GetMapping(value = "certificates/{id}")
     public GiftCertificate findById(@PathVariable Long id) {
-        //todo create new exception
         return giftCertificateService.findCertificateById(id);
     }
 
