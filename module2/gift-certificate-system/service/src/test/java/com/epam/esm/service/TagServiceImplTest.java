289a45ba737e0585b2c3b39500169d7583ea7aa9
Sergei Kristev <kristevsm@gmail.com>
2020-10-19 12:55:43 +0300

    Created service test

--- module2/gift-certificate-system/service/src/test/java/com/epam/esm/service/TagServiceImplTest.java
+++ module2/gift-certificate-system/service/src/test/java/com/epam/esm/service/TagServiceImplTest.java
@@ -1,26 +1,43 @@
 package com.epam.esm.service;
 
+import com.epam.esm.dao.GiftCertificateDao;
 import com.epam.esm.dao.TagDao;
+import com.epam.esm.dao.TagDaoJdbc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.sql.DataSource;
+
 @ExtendWith(SpringExtension.class)
-//@ContextConfiguration(classes = SpringConfig.class)
+@ContextConfiguration(locations = {"classpath:test-db.xml"})
 class TagServiceImplTest {
 
+    private GiftCertificateService certificateService;
+
     @Autowired
-    TagService tagService;
+    private DataSource dataSource;
 
     @Autowired
     private TagDao tagDao;
 
+    @Autowired
+    private GiftCertificateDao giftCertificateDao;
+
     @BeforeEach
     void setUp() {
-
+        certificateService = new GiftCertificateServiceImpl(giftCertificateDao, tagDao);
+        tagDao = new TagDaoJdbc(dataSource);
     }
 
+    @Test
+    public void test(){
+        System.out.println(certificateService.findAllCertificates());
+    }
 
 }
