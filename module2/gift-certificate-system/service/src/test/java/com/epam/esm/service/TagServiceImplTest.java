54cdc72d3fe9828c3dad7ebc067861975e7fb429
Sergei Kristev <kristevsm@gmail.com>
2020-10-20 19:01:02 +0300

    Fix certificate controller: added validation logic

--- module2/gift-certificate-system/service/src/test/java/com/epam/esm/service/TagServiceImplTest.java
+++ module2/gift-certificate-system/service/src/test/java/com/epam/esm/service/TagServiceImplTest.java
@@ -3,6 +3,9 @@ package com.epam.esm.service;
 import com.epam.esm.dao.GiftCertificateDao;
 import com.epam.esm.dao.TagDao;
 import com.epam.esm.dao.TagDaoJdbc;
+import com.epam.esm.exception.GiftCertificateNotFoundException;
+import com.epam.esm.exception.TagNotFoundException;
+import com.epam.esm.model.GiftCertificate;
 import com.epam.esm.model.Tag;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,13 +15,18 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"classpath:test-db.xml"})
 class TagServiceImplTest {
 
-    private GiftCertificateService certificateService;
+    private TagService tagService;
 
     @Autowired
     private DataSource dataSource;
@@ -31,21 +39,42 @@ class TagServiceImplTest {
 
     @BeforeEach
     void setUp() {
-        certificateService = new GiftCertificateServiceImpl(giftCertificateDao, tagDao);
+        tagService = new TagServiceImpl(tagDao);
         tagDao = new TagDaoJdbc(dataSource);
     }
 
     @Test
     void shouldFindAllTags() {
-        List<Tag> tagList = tagDao.findAll();
+
+        List<Tag> tags = tagService.findAllTags();
+        int expectedListSize = 3;
+        System.out.println(tags);
+        assertEquals(expectedListSize, tags.size());
     }
 
     @Test
-    void findTagById() {
+    void shouldFindTagById() {
+        Long expectedId = 1L;
+        Tag tag = tagService.findTagById(expectedId);
+        assertEquals(expectedId, tag.getId());
+        assertThrows(TagNotFoundException.class, () -> {
+            tagService.findTagById(12L);
+        });
     }
 
     @Test
-    void saveTag() {
+    void shouldSaveAndRemoveTag() {
+        Tag tag = Tag.builder()
+                .name("Tag 5")
+                .build();
+
+        Long tagId = tagService.saveTag(tag);
+        Tag actualTag = tagService.findTagById(tagId);
+        assertEquals(tagId, actualTag.getId());
+        tagService.deleteTag(tagId);
+        assertThrows(TagNotFoundException.class, () -> {
+            tagService.findTagById(5L);
+        });
     }
 
     @Test
