061f973167ca53bed104639688733826b1c2022d
Sergei Kristev <kristevsm@gmail.com>
2020-10-21 21:02:33 +0300

    Refactor WepAppInitializer

--- module2/gift-certificate-system/web/src/main/java/com/epam/esm/config/WebAppInitializer.java
+++ module2/gift-certificate-system/web/src/main/java/com/epam/esm/config/WebAppInitializer.java
@@ -1,23 +1,59 @@
 package com.epam.esm.config;
 
-import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-public class WebAppInitializer implements WebApplicationInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-//        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
-//        applicationContext.register(SpringConfig.class);
-//        applicationContext.setServletContext(servletContext);
-//
-//        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(servletContext));
-//        dispatcher.setLoadOnStartup(1);
-//        dispatcher.addMapping("/");
+        //create the root Spring application context
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(SpringConfig.class);
+
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+
+        //Create the dispatcher servlet's Spring application context
+        AnnotationConfigWebApplicationContext servletAppContext = new AnnotationConfigWebApplicationContext();
+        servletAppContext.register(SpringConfig.class);
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(servletAppContext);
+        // throw NoHandlerFoundException to controller ExceptionHandler.class. Used for <error-page> analogue
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+
+        //register and map the dispatcher servlet
+        //note Dispatcher servlet with constructor arguments
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+
+        FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encoding-filter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
     }
 
 }
